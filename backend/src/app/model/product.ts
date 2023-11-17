859d2e0d6b051ff17242689c7cd9df2b248c1d33
JanLayola <gilajan@protonmail.com>
2023-11-17 20:11:19 +0100

    Add IProduct interface

--- backend/src/app/model/product.ts
+++ backend/src/app/model/product.ts
@@ -1,11 +1,23 @@
 import * as mongoose from 'mongoose';
+import { Document, Schema } from "mongoose";
 
-const ProductSchema = new mongoose.Schema(
+export interface IProduct extends Document {
+        gtin: number;
+        name: string;
+        image: string;
+        brandName: string;
+        category: string;
+        color: string;
+        stock: number;
+        price: number;
+}
+
+const ProductSchema: Schema = new mongoose.Schema<IProduct>(
     {
         gtin: { type: Number, required: true },
         name: { type: String, required: true },
         image: { type: String },
-        brand: { type: String, required: true },
+        brandName: { type: String, required: true },
         category: { type: String, required: true },
         color: { type: String, required: true },
         stock: { type: Number, required: true },
